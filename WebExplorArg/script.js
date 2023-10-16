229311c437841a13b9dee9e19ae7232aa5cfc199
Maru <117225183+maeudi@users.noreply.github.com>
2023-10-16 15:10:45 -0300

    guia actual 16-10

--- WebExplorArg/script.js
+++ WebExplorArg/script.js
@@ -2,3 +2,15 @@ var adultos = Number.MIN_VALUE=0
 var children = Number.MIN_VALUE=0
 
 //verificar la fecha
+//fecha_vuelta >= fecha_ida
+
+//Datos del formulario
+document.querySelector('form')
+    form[0].addEventListener('submit', e=>{
+    e.preventDefault()
+    const data = Object.fromEntries(
+        new FormData(e.target)
+    )
+    alert(JSON.stringify(data))
+    }
+    )
