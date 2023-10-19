ec4fd48aabdc6661748551611d9fb969654bd129
Maru <117225183+maeudi@users.noreply.github.com>
2023-10-18 22:24:42 -0300

    actualiz links

--- WebExplorArg/script.js
+++ WebExplorArg/script.js
@@ -1,7 +1,7 @@
 //Enviar Formulario
 function sendMail(){
     var email = "contacto@ExplorArg.com";
-    var subject = "asunto";
+    var subject = "Asunto -Guia de Viaje";
     var msgBody = "Gracias por enviarnos un mail, nos pondremos en contacto en cuanto sea posible.";
     window.open(`mailto:${email}?subject=${subject}&body=${msgBody}`);
   }
@@ -15,17 +15,24 @@ function sendMail(){
     });
 
 //Validar Fechas
-var f1= fecha_ida (Date)
-var f2 = fecha_vuelta (Date)
+var f1
+var f2
 if(f1 < f2){
   console.log("Fecha Correcta");
 }
 
-if (f1.getTime() == f2.getTime()){
-    console.log("Son la misma fecha");
+if (f1 == f2){
+  console.log("Son la misma fecha");
 }
 
 if(f1 > f2){
   console.log("Fecha Incorrecta");
   window.MessageEvent("La fecha de vuelta no puede ser anterior")
+}
+//Links de lista select
+function destino() {
+  var url = document.regiones.region.options[document.regiones.region.selectedIndex].value
+  if (url != "...") {
+    window.location = url;
+  }
 }
