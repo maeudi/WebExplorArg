1fe495abeafdb92d7027ced9cef79f0a14797560
Maru <117225183+maeudi@users.noreply.github.com>
2023-10-21 12:23:27 -0300

    listado regiones

--- WebExplorArg/script.js
+++ WebExplorArg/script.js
@@ -4,6 +4,12 @@ function sendMail(){
     var subject = "Asunto -Guia de Viaje";
     var msgBody = "Gracias por enviarnos un mail, nos pondremos en contacto en cuanto sea posible.";
     window.open(`mailto:${email}?subject=${subject}&body=${msgBody}`);
+    input.addEventListener("keypress", function(event) {
+      if (event.key === "Enviar") {
+        event.preventDefault();
+        document.getElementById("enviar").click();
+      }
+    });
   }
 
 //Limpiar Formulario
@@ -29,10 +35,22 @@ if(f1 > f2){
   console.log("Fecha Incorrecta");
   window.MessageEvent("La fecha de vuelta no puede ser anterior")
 }
-//Links de lista select
-function destino() {
-  var url = document.regiones.region.options[document.regiones.region.selectedIndex].value
-  if (url != "...") {
-    window.location = url;
+//Lista "Viaja con nosotros"
+//Abrir la lista
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Cerrar la lista
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
   }
 }
