b2e3a14ccd7b8640ef8cda11a57f9e3462eb777f
Maria Eugenia Diaz <117225183+maeudi@users.noreply.github.com>
2023-10-21 19:12:01 -0300

    para main

---
+++ script.js
@@ -0,0 +1,56 @@
+//Enviar Formulario
+function sendMail(){
+    var email = "contacto@ExplorArg.com";
+    var subject = "Asunto -Guia de Viaje";
+    var msgBody = "Gracias por enviarnos un mail, nos pondremos en contacto en cuanto sea posible.";
+    window.open(`mailto:${email}?subject=${subject}&body=${msgBody}`);
+    input.addEventListener("keypress", function(event) {
+      if (event.key === "Enviar") {
+        event.preventDefault();
+        document.getElementById("enviar").click();
+      }
+    });
+  }
+
+//Limpiar Formulario
+  document.addEventListener('DOMContentLoaded', function(){
+    let formulario = document.getElementById('form');
+    formulario.addEventListener('submit', function() {
+    formulario.reset();
+    });
+    });
+
+//Validar Fechas
+var f1
+var f2
+if(f1 < f2){
+  console.log("Fecha Correcta");
+}
+
+if (f1 == f2){
+  console.log("Son la misma fecha");
+}
+
+if(f1 > f2){
+  console.log("Fecha Incorrecta");
+  alert("La fecha de vuelta no puede ser anterior")
+}
+//Lista "Viaja con nosotros"
+//Abrir la lista
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Cerrar la lista
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
